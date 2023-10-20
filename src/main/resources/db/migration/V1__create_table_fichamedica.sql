f2518ba752048ca030dd24f2b9c52b112d9f0b76
miguel <miguelrighi11@gmail.com>
2023-10-20 17:40:47 -0300

    ba

--- src/main/resources/db/migration/V1__create_table_fichamedica.sql
+++ src/main/resources/db/migration/V1__create_table_fichamedica.sql
@@ -1,4 +1,10 @@
 CREATE TABLE `fichamedica` (
- `id` bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
-
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `medico` VARCHAR(255) NOT NULL,
+  `paciente` VARCHAR(255) NOT NULL,
+  `dataMarcada` DATE NOT NULL,
+  `descricao` VARCHAR(255) DEFAULT NULL,
+  `sala` BIGINT NOT NULL
 );
+
+
--- src/main/resources/db/migration/V3__create_table_sala.sql
+++ src/main/resources/db/migration/V3__create_table_sala.sql
@@ -1,3 +1,10 @@
 CREATE TABLE `sala` (
- `id` bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ `id` BIGINT NOT NULL AUTO_INCREMENT,
+ `numero` BIGINT NOT NULL,
+ `bloco` VARCHAR(255) NOT NULL,
+ `fichamedica_id` BIGINT NOT NULL,
+ `usuario_id` BIGINT NOT NULL,
+ PRIMARY KEY (`id`),
+ FOREIGN KEY (`fichamedica_id`) REFERENCES `fichamedica` (`id`),
+ FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
 );
